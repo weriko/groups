a61e03fa26391c3641805606d1ef56fb7d8f81fc
weriko <53843483+weriko@users.noreply.github.com>
2021-09-16 22:38:48 -0500

    Update and rename diagram to Diagram.py

--- groups/Diagram.py
+++ groups/Diagram.py
@@ -9,10 +9,12 @@ class Diagram:
     def powerset(iterable):
         s = list(iterable)
         return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(1,len(s)+1))
-
+ 
     
     @staticmethod
-    def create_diagram(n, rad = 2,center=5, size=(20,20), circlerad = 0.3):
+    def create_diagram(n, rad = 2,center=5, size=(25,25), circlerad = 0.3, names=None, quantities=None):
+        #If used by a group it will accept a powerset of all the intersections of said groups, plotting their
+        #quantities
         plt.rcParams["figure.figsize"] = size
         fig, ax = plt.subplots()
         angle = 2 * np.pi / n
@@ -21,23 +23,25 @@ class Diagram:
         for i in xy:
             circle = plt.Circle(i,rad+circlerad, fill=False)
             ax.add_patch(circle)
-        xy_name = [(i, x[0], x[1]) for i,x in enumerate(xy)]
-
+        if names is None:
+            names = range(len(xy))
+        if quantities is None:
+            quantities = {}
+       
+        xy_name = [(i, x[0], x[1]) for i,x in zip(names,xy)]
+  
         all_circ = Diagram.powerset(xy_name)
-
         intersections = {}
-
+        
         for x in all_circ:
+            
             name = tuple(i[0] for i in x)
             xs = sum([i[1] for i in x])/len(x)
             ys = sum([i[2] for i in x])/len(x)
 
-
-
-            plt.annotate(name, (xs, ys))
+            
+            plt.annotate("".join(str(name))+" "+str(quantities.get(frozenset(name),"")),
+                         (xs, ys))
             plt.scatter(xs,ys,label=name)
-
-
-
         plt.show()
         
